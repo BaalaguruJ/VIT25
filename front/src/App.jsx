358114e49f6845f5c75d97e3ca24084948df1983
Gogul11 <gogu0811@gmail.com>
2025-04-01 15:51:25 +0530

    Installation

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,16 +1,44 @@
 import{BrowserRouter, Route, Routes} from 'react-router-dom'
 import Home from './pages/home'
 import Rover from './pages/rover'
+import { useState, useEffect } from 'react'
+import {io} from 'socket.io-client'
+import axios from 'axios'
 
 function App() {
 
+
+  const [id, setId] = useState(null)
+  
+useEffect(() => {
+  const socket = io('http://localhost:3000')
+
+  socket.on('connect', () => {
+    setId(socket.id)
+  })
+  socket.on('getSession', async(id) => {
+    console.log(id)
+  })
+  socket.on('roverPosition', (data) => {
+    console.log(data)
+  })
+
+  // Cleanup the socket connection on unmount
+  return () => {
+    socket.disconnect()
+  }
+}, [])
+
   return (
-    <BrowserRouter>
-        <Routes>
-          <Route index element={<Home/>}/>
-          <Route path='/rover' element={<Rover/>}/>
-        </Routes>
-    </BrowserRouter>
+    // <BrowserRouter>
+    //     <Routes>
+    //       <Route index element={<Home/>}/>
+    //       <Route path='/rover' element={<Rover/>}/>
+    //     </Routes>
+    // </BrowserRouter>
+    <>
+    {id && <p>{id}</p>}
+    </>
   )
 }
 
