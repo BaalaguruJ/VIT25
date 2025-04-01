05e551934b0291dba386ef6a4224742e13bbdab8
Gogul11 <gogu0811@gmail.com>
2025-04-01 11:19:10 +0530

    Installation

---
+++ front/src/App.jsx
@@ -0,0 +1,17 @@
+import{BrowserRouter, Route, Routes} from 'react-router-dom'
+import Home from './pages/home'
+import Rover from './pages/rover'
+
+function App() {
+
+  return (
+    <BrowserRouter>
+        <Routes>
+          <Route index element={<Home/>}/>
+          <Route path='/rover' element={<Rover/>}/>
+        </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
---
+++ front/src/pages/home.jsx
@@ -0,0 +1,7 @@
+export default function Home(){
+    return(
+        <div className="text-blue-600">
+            This is home page
+        </div>
+    )
+}
