41b0805aee87a96edcd937c6d202ce742a49eded
Gogul11 <gogu0811@gmail.com>
2025-05-03 21:37:55 +0530

    final2

--- front/src/socket.js
+++ front/src/socket.js
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client';
 
-const socket = io('http://localhost:3000', {
+const socket = io('https://cropcare-r7hr.onrender.com', {
   autoConnect: false,  // Prevent auto connection
 });
 
